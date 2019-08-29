18fc7d059f86caee3b32c02d43ff221c8c2d11f1
JesseHoch <jesse@admins-MacBook-Pro.local>
2019-08-29 16:11:58 -0400

    Changes for second round of testing

--- scripts/deployOKD.sh
+++ scripts/deployOKD.sh
@@ -26,7 +26,7 @@ echo $(date) " - Ansible, pyOpenSSL and python-passlib successfully installed"
 
 echo $(date) " - Installing OKD packages, openshift-ansible, and docker"
 
-yum -y install centos-release-openshift-origin
+yum -y install centos-release-openshift-origin39
 yum -y install openshift-ansible
 yum -y install docker
 
@@ -34,7 +34,7 @@ echo $(date) " -  OKD packages, openshift-ansible, and dockersuccessfully instal
 
 echo $(date) " - Enabling and starting docker"
 
-sed -i -e "s#^OPTIONS='--selinux-enabled'#OPTIONS='--selinux-enabled --insecure-registry 172.30.0.0/16'#" /etc/sysconfig/docker
+sed -i -e "s#^OPTIONS='--selinux-enabled'#OPTIONS='--selinux-enabled --insecure-registry 104.209.0.0/16'#" /etc/sysconfig/docker
 systemctl enable docker-cleanup
 systemctl enable docker
 systemctl start docker
@@ -50,7 +50,8 @@ nodes
 ansible_ssh_user=azureuser
 ansible_become=yes
 debug_level=2
-deployment_type=origin
+openshift_deployment_type=origin
+
 openshift_master_identity_providers=[{'name': 'htpasswd_auth', 'login': 'true', 'challenge': 'true', 'kind': 'HTPasswdPasswordIdentityProvider', 'filename': '/etc/origin/master/htpasswd'}]
 
 openshift_master_cluster_method=native
@@ -74,7 +75,7 @@ appnodeVM-0 openshift_node_labels="{'region': 'primary', 'zone': 'default'}"
 infraVM-0 openshift_node_labels="{'region': 'infra', 'zone': 'default'}"
 EOF
 
-ansible-playbook /usr/share/ansible/openshift-ansible/playbook/prerequisites.yml
-ansible-playbook /usr/share/ansible/openshift-ansible/playbook/deploy_cluster.yml
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
 
 echo $(date) " - Script Complete"

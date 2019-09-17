50c6756baa532ec7b7974831ce49030fea1c693d
JesseHoch <jesse@admins-MacBook-Pro.local>
2019-09-17 11:17:29 -0400

    Added to script

--- scripts/bastionPrep.sh
+++ scripts/bastionPrep.sh
@@ -3,9 +3,10 @@
 echo $(date) " - Starting Script"
 
 # Update system to latest packages and install dependencies
-echo $(date) " - Update system to latest packages"
+echo $(date) " - Update system to latest packages and install dependencies"
 
 yum -y update
+yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct httpd-tools
 
 echo $(date) " - System updates successfully installed"
 
@@ -25,6 +26,7 @@ DHCP_HOSTNAME=bastionVM-0
 EOF
 
 echo $(date) " - Changed interface setting to NM_CONTROLLED=yes "
+systemctl restart network
 
 echo $(date) " - Adding entries to host file"
 
@@ -33,7 +35,7 @@ echo "10.10.1.10 masterVM-0  master.example.xip.io   okd.master.example.xip.io" 
 echo "10.10.1.11 infraVM-0   infra.example.xip.io    apps.okd.infra.example.xip.io" >> /etc/hosts
 echo "10.10.1.12 appnodeVM-0 node.example.xip.io" >> /etc/hosts
 
-echo $(date) " -Entries added to host file"
+echo $(date) " - Entries added to host file"
 
 echo $(date) " - Adding SSH keys"
 
@@ -41,15 +43,30 @@ wget https://raw.githubusercontent.com/linuxacademy/content-openshift-origin-azu
 
 wget https://raw.githubusercontent.com/linuxacademy/content-openshift-origin-azure/master/ssh/id_rsa.pub -P /home/azureuser/.ssh/
 
-chown azureuser:azureuser /home/azureuser/.ssh/id_rsa*
+chown -R azureuser:azureuser /home/azureuser/.ssh/id_rsa*
 chmod 600 /home/azureuser/.ssh/id_rsa*
 
-ssh -o StrictHostKeyChecking=no azureuser@master.example.xip.io uname -a
+ssh -o StrictHostKeyChecking=no master.example.xip.io uname -a
 
-ssh -o StrictHostKeyChecking=no azureuser@infra.example.xip.io uname -a
+ssh -o StrictHostKeyChecking=no infra.example.xip.io uname -a
 
-ssh -o StrictHostKeyChecking=no azureuser@node.example.xip.io uname -a
+ssh -o StrictHostKeyChecking=no @node.example.xip.io uname -a
 
 echo $(date) " - SSH keys added"
+
+# Install EPEL repository
+echo $(date) " - Installing EPEL"
+
+yum -y install epel-release
+sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+
+echo $(date) " - EPEL successfully installed"
+
+# Installation Ansible, pyOpenSSL and python-passlib
+echo $(date) " - Installing Ansible, pyOpenSSL and python-passlib"
+
+yum -y --enablerepo=epel install centos-release-ansible26 openssl-devel python-devel
+
+echo $(date) " - Ansible, pyOpenSSL and python-passlib successfully installed"
 
 echo $(date) " - Script Complete"
